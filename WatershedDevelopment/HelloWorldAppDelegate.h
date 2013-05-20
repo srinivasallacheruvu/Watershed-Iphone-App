a1132ff2ed559ddd9442a6893a69cc76800664c7
Karthik <namburu.karthik@gmail.com>
2013-05-20 08:31:23 +0530

    Initial Commit

---
+++ WatershedDevelopment/HelloWorldAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  HelloWorldAppDelegate.h
+//  WatershedDevelopment
+//
+//  Created by Karthik on 20/05/13.
+//  Copyright (c) 2013 Karthik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class HelloWorldViewController;
+
+@interface HelloWorldAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) HelloWorldViewController *viewController;
+
+@end
---
+++ WatershedDevelopment/HelloWorldViewController.h
@@ -0,0 +1,13 @@
+//
+//  HelloWorldViewController.h
+//  WatershedDevelopment
+//
+//  Created by Karthik on 20/05/13.
+//  Copyright (c) 2013 Karthik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController
+
+@end
